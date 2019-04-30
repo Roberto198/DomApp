bf1fb034f2e0f292971517c3d5d4a0b3707013c4
HassanBharu <hassan_7_8_6@hotmail.com>
2019-04-30 14:00:07 +0100

    done checkbox

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
 // const listItem = document.querySelectorAll('li');
 const listItems = [...document.getElementsByClassName('listItem')];
-const input = document.getElementById('formBox');
+const input = document.getElementById('formData');
+const boxInput = document.getElementById('formBox');
+const list = document.getElementById('list')
+const chkBoxs = [...document.getElementsByClassName('chkBox')];
 
-listItems.forEach(listItem => {
-	listItem.addEventListener('click', changeStatus);
-
-	function changeStatus() {
-		if (listItem.style.textDecorationLine === '') {
-			listItem.style.textDecorationLine = 'line-through';
-		} else listItem.style.textDecorationLine = '';
-	}
-});
-// function inputData() {
-// 	console.log('hello');
-// 	console.log(input.value);
-// 	console.log(document.getElementById('formData').value);
-// }
+console.log(chkBoxs)
 
 function func(e) {
 	e.preventDefault();
-	console.log(input.value);
+
+	if (boxInput.value) {
+		const newListItem = document.createElement('li')
+		newListItem.setAttribute('class', 'listItem')
+
+		newListItem.innerHTML = (`<p><input type="checkbox" />${boxInput.value}</p>`)
+
+		newListItem.addEventListener('click', changeStatus);
+
+		list.appendChild(newListItem)
+	}
+
 }
 input.addEventListener('click', func);
+
+
+
+listItems.forEach(listItem => {
+	listItem.addEventListener('click', changeStatus);
+});
+
+chkBoxs.forEach(chkBox => {
+	chkBox.addEventListener('click', checked)
+})
+
+function checked() {
+	if (this.checked === 'checked') {
+		this.checked = false
+		this.parentNode.textDecorationLine = ''
+	}
+}
+
+
+function changeStatus() {
+	//console.log(this.childNodes[1].childNodes[0])
+	if (this.style.textDecorationLine === '') {
+		this.style.textDecorationLine = 'line-through';
+		this.childNodes[1].childNodes[0].checked = 'checked'
+
+
+	} else {
+		this.style.textDecorationLine = '';
+		this.childNodes[1].childNodes[0].checked = false
+
+	}
+}
+
+
